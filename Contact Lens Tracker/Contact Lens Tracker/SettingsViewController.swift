151a8b0c0ac8d0ddbda85c705eec12288a7acf50
Jaden Geller <jaden.geller@gmail.com>
2014-11-22 18:29:52 -0800

    Added logic to steppers

--- Contact Lens Tracker/Contact Lens Tracker/SettingsViewController.swift
+++ Contact Lens Tracker/Contact Lens Tracker/SettingsViewController.swift
@@ -9,13 +9,35 @@
 import UIKit
 
 class SettingsViewController: UITableViewController {
+    @IBOutlet weak var remainingStepper: UIStepper!
+    
+    var remainingDays: Int = 365 {
+        didSet {
+            let day = remainingDays == 1 ? "Day" : "Days"
+            remainingLabel.text = "\(remainingDays) \(day) Remaining"
+        }
+    }
+    
+    var lifetimeDays: Int = 400 {
+        didSet {
+            newLifetimeLabel.text = "\(lifetimeDays) Day Lifetime"
+            
+            if (lifetimeDays < remainingDays) {
+                remainingDays = lifetimeDays
+            }
+            
+            remainingStepper.maximumValue = Double(lifetimeDays)
+        }
+    }
+    
     @IBOutlet weak var remainingLabel: UILabel!
     @IBAction func remainingChanged(sender: UIStepper) {
-        
+        remainingDays = Int(sender.value)
     }
    
     @IBOutlet weak var newLifetimeLabel: UILabel!
     @IBAction func newLifetimeChanged(sender: UIStepper) {
+        lifetimeDays = Int(sender.value)
     }
     @IBAction func donePress(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
